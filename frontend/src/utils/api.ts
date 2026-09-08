e918e3f1a2ef35f70959f0317f2ce19276f9d3e1
ajay singh <ajaysinghbanafer1@gmail.com>
2026-09-08 12:56:09 +0530

    update backend url

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Backend base URL
+  baseURL: 'https://onestall-cargo.onrender.com/api', // Backend base URL
   headers: {
     'Content-Type': 'application/json',
   },
